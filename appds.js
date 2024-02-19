192facd203bc622bbcf886f9f6183b3f135ef2a5
鲸鱼东 <116928040+xudondon888@users.noreply.github.com>
2024-02-19 13:31:17 +0800

    更新 appds.js

--- appds.js
+++ appds.js
@@ -1,11 +1,9 @@
-/*
-[rewrite_local]
 
+[rewrite_local]
 https://app.appds.cn/appstore url script-request-header https://raw.githubusercontent.com/xudondon888/Rewrite/main/appds.js
 
 [mitm]
 hostname = app.appds.cn
-*/
 
 var updatedUrl = $request.url.replace(/udid=[^&]*/, 'udid=00008120-000A14A01EE0A01E');
 $done({ url: updatedUrl });
