dee1cf311f7525aa47a1754724a287347ca24403
鲸鱼东 <116928040+xudondon888@users.noreply.github.com>
2024-02-19 22:53:25 +0800

    更新 wyy.js

--- wyy.js
+++ wyy.js
@@ -1,3 +1,5 @@
+
+/*
 [rewrite_local]
 ^https:\/\/interface3?\.music\.163\.com\/eapi\/playermode\/ url script-request-header https://gist.githubusercontent.com/Sliverkiss/479ecf770801bb8d3efa514c56a699e7/raw/WyyCrack.js
 
@@ -15,3 +17,22 @@
 
 [mitm]
 hostname = *.music.163.com
+*/
+
+let header = $request.headers;
+const isQuanX = typeof $task !== "undefined";
+
+if (isQuanX) {
+  header["MConfig-Info"] = '你的数据';
+  header["User-Agent"] = '你的数据';
+  header["Cookie"] = '你的数据';
+} else {
+  headers["mconfig-info"] = '你的数据';
+  headers["user-agent"] = '你的数据';
+  headers["cookie"] = '你的数据';
+}
+
+$done({ headers: header });
+
+
+
