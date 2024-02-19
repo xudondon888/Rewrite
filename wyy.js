41ed7496a496b3b7a24317a6ac1becdbebe125ec
鲸鱼东 <116928040+xudondon888@users.noreply.github.com>
2024-02-20 00:19:28 +0800

    更新 wyy.js

--- wyy.js
+++ wyy.js
@@ -6,24 +6,24 @@ VIP音乐试听权限+
 [rewrite_local]
 
 # 播放器会员皮肤
-^https:\/\/interface3?\.music\.163\.com\/eapi\/playermode\/ url script-request-header wyy.js
+^https:\/\/interface3?\.music\.163\.com\/eapi\/playermode\/ url script-request-header https://raw.githubusercontent.com/xudondon888/Rewrite/main/wyy.js
 
 # 搜索结果会员歌曲
-^https:\/\/interface3?\.music\.163\.com\/eapi\/search\/complex\/(page|rec\/song\/get) url script-request-header wyy.js
+^https:\/\/interface3?\.music\.163\.com\/eapi\/search\/complex\/(page|rec\/song\/get) url script-request-header https://raw.githubusercontent.com/xudondon888/Rewrite/main/wyy.js
 
 # 播放器会员歌曲
 ^https:\/\/interface3?\.music\.163\.com\/eapi\/v3\/song\/detail url script-request-header wyy.js
-^https:\/\/interface3?\.music\.163\.com\/eapi\/song\/(chorus|enhance\/|play\/|type\/detail\/get) url script-request-header wyy.js
-^https:\/\/interface3?\.music\.163\.com\/eapi\/(v1\/artist\/top\/song|v3\/discovery\/recommend\/songs) url script-request-header wyy.js
+^https:\/\/interface3?\.music\.163\.com\/eapi\/song\/(chorus|enhance\/|play\/|type\/detail\/get) url script-request-header https://raw.githubusercontent.com/xudondon888/Rewrite/main/wyy.js
+^https:\/\/interface3?\.music\.163\.com\/eapi\/(v1\/artist\/top\/song|v3\/discovery\/recommend\/songs) url script-request-header https://raw.githubusercontent.com/xudondon888/Rewrite/main/wyy.js
 
 # 侧边栏会员等级
-^https:\/\/interface3?\.music\.163\.com\/eapi\/vipnewcenter\/app\/resource\/newaccountpage url script-request-header wyy.js
+^https:\/\/interface3?\.music\.163\.com\/eapi\/vipnewcenter\/app\/resource\/newaccountpage url script-request-header https://raw.githubusercontent.com/xudondon888/Rewrite/main/wyy.js
 
 # 首页歌单会员歌曲
-^https?:\/\/interface3?\.music\.163\.com\/eapi\/(homepage\/|v6\/)?playlist\/ url script-request-header wyy.js
+^https?:\/\/interface3?\.music\.163\.com\/eapi\/(homepage\/|v6\/)?playlist\/ url script-request-header https://raw.githubusercontent.com/xudondon888/Rewrite/main/wyy.js
 
 # 会员认证
-^https?:\/\/interface3?\.music\.163\.com\/eapi\/vipauth\/app\/auth\/(soundquality\/)?query url script-request-header wyy.js
+^https?:\/\/interface3?\.music\.163\.com\/eapi\/vipauth\/app\/auth\/(soundquality\/)?query url script-request-header https://raw.githubusercontent.com/xudondon888/Rewrite/main/wyy.js
 
 
 [mitm] 
