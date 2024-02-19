c31fbca4f210c6439bb87055190442bac2c14e0d
鲸鱼东 <116928040+xudondon888@users.noreply.github.com>
2024-02-19 12:52:49 +0800

    更新 appds.js

---
+++ appds.js
@@ -0,0 +1,11 @@
+/*
+[rewrite_local]
+
+https://afzs.iosoi.cn/appstore url script-request-header https://raw.githubusercontent.com/Yu9191/Rewrite/main/afzs.js
+
+[mitm]
+hostname = app.appds.cn
+*/
+
+var updatedUrl = $request.url.replace(/udid=[^&]*/, 'udid=00008120-000A14A01EE0A01E');
+$done({ url: updatedUrl });
