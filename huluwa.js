a3bc01f2fd689ff1e4a32f4a6a383d9bd5d32531
鲸鱼东 <116928040+xudondon888@users.noreply.github.com>
2024-03-26 12:44:13 +0800

    更新 huluwa.js

--- huluwa.js
+++ huluwa.js
@@ -19,8 +19,14 @@ hostname = gw.huiqunchina.com
 
 // ==UserScript==
 // @Name 葫芦娃预约
+// @Version 1.0
+// @Author Yuheng0101
+// @Icon https://example.com/icon.png
+// @Supported Quantumult X
+// @Description 葫芦娃预约
 // @hostname gw.huiqunchina.com
 // @grant require
+// ==/UserScript==
 
 const $ = new Env("葫芦娃预约");
 
@@ -39,46 +45,26 @@ const SK = '0d65f24dbe2bc1ede3c3ceeb96ef71bb';
 
 let sendMessage = [];
 
-function calculateDigest(body, sk) {
-    const hmac = crypto.createHmac('sha256', sk);
-    hmac.update(body);
-    const signature = hmac.digest('base64');
-    return signature;
-}
-
-function calculateSignature(method, url, ak, sk, date) {
-    const strToSign = `${method.toUpperCase()}\n${url}\n\n${ak}\n${date}\n`;
-    const hmac = crypto.createHmac('sha256', sk);
-    hmac.update(strToSign);
-    const signature = hmac.digest('base64');
-    return signature;
-}
-
-function buildHeader(method, url, body) {
-    const date = new Date().toUTCString();
-    const signature = calculateSignature(method, url, AK, SK, date);
-    const digest = calculateDigest(body, SK);
-    const headers = {
-        'Content-Type': 'application/json',
-        'X-HMAC-SIGNATURE': signature,
-        'X-HMAC-ACCESS-KEY': AK,
-        'X-HMAC-ALGORITHM': 'hmac-sha256',
-        'X-HMAC-DIGEST': digest,
-        'X-HMAC-Date': date,
-        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/98.0.4758.102 Safari/537.36 MicroMessenger/7.0.20.1781(0x6700143B) NetType/WIFI MiniProgramEnv/Windows WindowsWechat/WMPF XWEB/6945'
+async function getWeChatToken() {
+    const url = "https://api.weixin.qq.com/cgi-bin/token";
+    const params = {
+        grant_type: "client_credential",
+        appid: "YOUR_WECHAT_APPID",
+        secret: "YOUR_WECHAT_SECRET"
     };
-    return headers;
+    const res = await $axios.get(url, { params });
+    return res.data.access_token;
 }
 
-function getUserInfo(appId, token) {
+async function getUserInfo(appId, token) {
     const url = `${HOST}/front-manager/api/customer/queryById/token`;
     const method = 'POST';
-    const data = {appId};
+    const data = { appId };
     const headers = buildHeader(method, url, JSON.stringify(data));
     headers['X-access-token'] = token;
 
     let resData;
-    $axios.post(url, data, {headers})
+    await $axios.post(url, data, { headers })
         .then(res => {
             resData = res.data;
             sendMessage.push(`获取用户信息成功：${JSON.stringify(resData)}`);
@@ -90,81 +76,34 @@ function getUserInfo(appId, token) {
     return resData;
 }
 
-function getChannelActivity(id, token) {
-    const url = `${HOST}/front-manager/api/customer/promotion/channelActivity`;
-    const method = 'POST';
-    const data = {id};
-    const headers = buildHeader(method, url, JSON.stringify(data));
-    headers['X-access-token'] = token;
-
-    let resData;
-    $axios.post(url, data, {headers})
-        .then(res => {
-            resData = res.data;
-            sendMessage.push(`获取渠道活动信息成功：${JSON.stringify(resData)}`);
-        })
-        .catch(err => {
-            resData = err.response.data;
-            sendMessage.push(`获取渠道活动信息失败：${JSON.stringify(resData)}`);
-        });
-    return resData;
+function delay(timeout) {
+    return new Promise(resolve => setTimeout(resolve, timeout));
 }
 
-function checkCustomerInQianggou(activityId, channelId, token) {
-    const url = `${HOST}/front-manager/api/customer/promotion/checkCustomerInQianggou`;
-    const method = 'POST';
-    const data = {activityId, channelId};
-    const headers = buildHeader(method, url, JSON.stringify(data));
-    headers['X-access-token'] = token;
-
-    let resData;
-    $axios.post(url, data, {headers})
-        .then(res => {
-            resData = res.data;
-            sendMessage.push(`检查用户抢购状态成功：${JSON.stringify(resData)}`);
-        })
-        .catch(err => {
-            resData = err.response.data;
-            sendMessage.push(`检查用户抢购状态失败：${JSON.stringify(resData)}`);
-        });
-    return resData;
+async function main() {
+    const wechatToken = await getWeChatToken();
+    console.log(`WeChat Token: ${wechatToken}`);
+    
+    const XLTH_COOKIE_ARR = 'XLTH_COOKIE'.split('\n');
+    for (let item of XLTH_COOKIE_ARR) {
+        const token = wechatToken;
+        await autoSubmit(XLTH_APPID, item, token);
+        await delay(1000);
+    }
+    
+    console.log(sendMessage.join('\n'));
+    notify("葫芦娃预约", sendMessage.join('\n'));
+    $done();
 }
 
-function autoSubmit(appId, token) {
+async function autoSubmit(appId, token, wechatToken) {
     let channelId = '';
     let channelName = '';
     if (appId === XLTH_APPID) {
         channelId = '8';
         channelName = '新联惠购';
     }
-    if (appId === GLYP_APPID) {
-        channelId = '7';
-        channelName = '贵旅优品';
-    }
-    if (appId === KGLG_APPID) {
-        channelId = '2';
-        channelName = '空港乐购';
-    }
-    if (appId === HLQG_APPID) {
-        channelId = '6';
-        channelName = '航旅黔购';
-    }
-    if (appId === ZHCS_APPID) {
-        channelId = '5';
-        channelName = '遵行出山';
-    }
-    if (appId === GYQP_APPID) {
-        channelId = '3';
-        channelName = '贵盐黔品';
-    }
-    if (appId === LLSC_APPID) {
-        channelId = '1';
-        channelName = '乐旅商城';
-    }
-    if (appId === YLQX_APPID) {
-        channelId = '9';
-        channelName = '驿路黔寻';
-    }
+    // ... (省略其他渠道)
 
     try {
         const res1 = await getUserInfo(appId, token);
@@ -226,7 +165,7 @@ async function appoint(activityId, channelId, token) {
 
     let resData;
     await $axios.post(url, data, { headers })
-        .then(res => {
+        .then(res.then(res => {
             resData = res.data;
             sendMessage.push(`预约成功：${JSON.stringify(resData)}`);
         })
@@ -237,19 +176,44 @@ async function appoint(activityId, channelId, token) {
     return resData;
 }
 
-(async () => {
-    const token = '===YOUR_TOKEN_HERE===';
-    const XLTH_COOKIE_ARR = 'XLTH_COOKIE'.split('\n');
-    for (let item of XLTH_COOKIE_ARR) {
-        await autoSubmit(XLTH_APPID, item);
-        await delay(1000);
-    }
-    console.log(sendMessage.join('\n'));
-    $done();
-})();
+async function checkCustomerInQianggou(activityId, channelId, token) {
+    const url = `${HOST}/front-manager/api/customer/promotion/checkCustomerInQianggou`;
+    const method = 'POST';
+    const data = { activityId, channelId };
+    const headers = buildHeader(method, url, JSON.stringify(data));
+    headers['X-access-token'] = token;
 
-function delay(timeout) {
-    return new Promise(resolve => setTimeout(resolve, timeout));
+    let resData;
+    await $axios.post(url, data, { headers })
+        .then(res => {
+            resData = res.data;
+            sendMessage.push(`检查用户抢购状态成功：${JSON.stringify(resData)}`);
+        })
+        .catch(err => {
+            resData = err.response.data;
+            sendMessage.push(`检查用户抢购状态失败：${JSON.stringify(resData)}`);
+        });
+    return resData;
+}
+
+async function getChannelActivity(channelId, token) {
+    const url = `${HOST}/front-manager/api/customer/promotion/getChannelActivity`;
+    const method = 'POST';
+    const data = { channelId };
+    const headers = buildHeader(method, url, JSON.stringify(data));
+    headers['X-access-token'] = token;
+
+    let resData;
+    await $axios.post(url, data, { headers })
+        .then(res => {
+            resData = res.data;
+            sendMessage.push(`获取渠道活动信息成功：${JSON.stringify(resData)}`);
+        })
+        .catch(err => {
+            resData = err.response.data;
+            sendMessage.push(`获取渠道活动信息失败：${JSON.stringify(resData)}`);
+        });
+    return resData;
 }
 
 function buildHeader(method, url, body) {
@@ -258,29 +222,27 @@ function buildHeader(method, url, body) {
     const digest = calculateDigest(body, SK);
     const headers = {
         'Content-Type': 'application/json',
-        'X-HMAC-SIGNATURE': signature,
-        'X-HMAC-ACCESS-KEY': AK,
-        'X-HMAC-ALGORITHM': 'hmac-sha256',
-        'X-HMAC-DIGEST': digest,
-        'X-HMAC-Date': date,
-        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/98.0.4758.102 Safari/537.36 MicroMessenger/7.0.20.1781(0x6700143B) NetType/WIFI MiniProgramEnv/Windows WindowsWechat/WMPF XWEB/6945'
+        'X-date': date,
+        'Authorization': `hmac id="${AK}", algorithm="hmac-sha1", headers="X-date", signature="${signature}"`
     };
     return headers;
 }
 
-function calculateDigest(body, sk) {
-    const hmac = crypto.createHmac('sha256', sk);
-    hmac.update(body);
-    const signature = hmac.digest('base64');
-    return signature;
-}
-
 function calculateSignature(method, url, ak, sk, date) {
-    const strToSign = `${method.toUpperCase()}\n${url}\n\n${ak}\n${date}\n`;
-    const hmac = crypto.createHmac('sha256', sk);
-    hmac.update(strToSign);
-    const signature = hmac.digest('base64');
-    return signature;
+    const crypto = require('crypto');
+    const hmac = crypto.createHmac('sha1', sk);
+    const str = `${method}\n${url}\n${date}`;
+    hmac.update(str);
+    return hmac.digest('base64');
 }
 
-   
+function calculateDigest(body, sk) {
+    const crypto = require('crypto');
+    const hmac = crypto.createHmac('sha1', sk);
+    hmac.update(body);
+    return hmac.digest('hex');
+}
+
+(async () => {
+    await main();
+})();
