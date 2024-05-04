cb61818bfc3c0c63d5dafa7b8e6e82c44a1051d2
鲸鱼东 <116928040+xudondon888@users.noreply.github.com>
2024-05-05 00:50:43 +0800

    更新 wyy.js

--- loon/wyy.js
+++ loon/wyy.js
@@ -1,9 +1,13 @@
 var headers = $request['headers'];
 delete headers["MConfig-Info"];
 delete headers["User-Agent"];
-delete headers["Cookie"];
+delete headers ["Cookie"];
 
-headers['music_MConfig-Info'] = $persistentStore.read("music_MConfig-Info");
-headers['music_User-Agent'] = $persistentStore.read("music_User-Agent");
-headers['music_Cookie'] = $persistentStore.read("music_Cookie");
+// headers['buvid'] = $persistentStore.read("music_buvid");
+headers['MConfig-Info'] = 
+$persistentStore.read("music_MConfig-Info");
+headers['User-Agent'] = 
+$persistentStore.read("music_User-Agent");
+headers['Cookie'] = 
+$persistentStore.read("music_Cookie");
 $done({ 'headers': headers });
