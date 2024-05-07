532d43d6a0218de0c9c47f187cdefcb7180bb9a9
鲸鱼东 <116928040+xudondon888@users.noreply.github.com>
2024-05-07 11:09:56 +0800

    更新 bili.js

--- loon/bili.js
+++ loon/bili.js
@@ -1,12 +1,16 @@
 var headers = $request.headers;
-var ck = $persistentStore.read("Bilibili__Cookie");
-var au = $persistentStore.read("Bilibili__Authorization");
 
-if (ck) {
+var ck = $persistentStore.read("Bilibili__Cookie");
+var au = $persistentStore.read("Bilibili__ Authorization");
+
+if (headers['Cookie'] != undefined) {
+    headers['Cookie'] = ck;
+} else {
     headers['cookie'] = ck;
 }
-
-if (au) {
+if (headers['Authorization'] != undefined) {
+    headers['Authorization'] = au;
+} else {
     headers['authorization'] = au;
 }
 
